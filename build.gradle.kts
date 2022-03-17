55bc754d285b8bb01107bc765a05a968290011ff
Michael Ventura <michael.ventura@gmail.com>
2022-03-17 10:15:49 -0400

    initial codename branding

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+description = "Phenix objects shared between modules"
+
+dependencies {
+  implementation("net.inetalliance:potion:6.1-SNAPSHOT")
+  implementation("net.inetalliance:types:1.1-SNAPSHOT")
+  implementation("net.inetalliance:cli:1.1-SNAPSHOT")
+  implementation("net.inetalliance:sql:1.1-SNAPSHOT")
+  implementation("net.inetalliance:log:1.1-SNAPSHOT")
+  implementation("net.inetalliance:funky:1.1-SNAPSHOT")
+  implementation("net.inetalliance:validation:1.1-SNAPSHOT")
+  implementation("org.apache.commons:commons-csv:1.9.0")
+  implementation("net.inetalliance.msg:bjx:6.1-SNAPSHOT")
+  implementation("org.postgresql:postgresql:42.3.3")
+}

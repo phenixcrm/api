776f8b87846f8af53677ae6c5403ad4bcb683a7c
Michael Ventura <michael.ventura@gmail.com>
2022-05-21 18:37:39 -0400

    hook in parent call to outbound-dial segment

--- src/main/java/com/ameriglide/phenix/api/VoiceStatus.java
+++ src/main/java/com/ameriglide/phenix/api/VoiceStatus.java
@@ -12,6 +12,8 @@ import net.inetalliance.log.Log;
 import net.inetalliance.potion.Locator;
 
 import java.time.temporal.ChronoUnit;
+import java.util.Objects;
+import java.util.stream.Stream;
 
 import static java.lang.String.join;
 import static java.time.LocalDateTime.now;
@@ -24,44 +26,38 @@ public class VoiceStatus extends PhenixServlet {
     var s = new StringBuilder("/twilio/voice/status:");
     request.getParameterMap().forEach((k, v) -> s.append("%n%s: %s".formatted(k, join(", ", v))));
     log.info(s);
-    var callSid = request.getParameter("CallSid");
-    var call = Locator.$(new Call(callSid));
-    if (call == null) {
-      log.warning("status update on call that isn't present: %s", callSid);
-    } else {
-      var segment = new Segment(call,Integer.parseInt(request.getParameter("SequenceNumber")));
-      if(!Locator.isRead(segment)) {
-        segment.setCreated(now());
-        Locator.create("VoiceStatus", segment);
-      }
-      switch (request.getParameter("CallbackSource")) {
-        case "call-progress-events" -> {
-          update(call, "VoiceStatus", callCopy -> {
-            switch (request.getParameter("CallStatus")) {
-              case "completed" -> {
-                update(segment,"VoiceStatus", segmentCopy ->  {
-                  segmentCopy.setEnded(now());
-                  if(segment.isAnswered()) {
-                    callCopy.setTalkTime(Funky.of(call.getTalkTime()).orElse(0L) + segment.getTalkTime());
-                  }
-                });
-                if (call.getResolution() == null) {
-                  callCopy.setResolution(Resolution.DROPPED);
-                }
-                callCopy.setDuration(ChronoUnit.SECONDS.between(call.getCreated(), segment.getEnded()));
+    var call = Stream.of(request.getParameter("CallSid"), request.getParameter("ParentCallSid"))
+      .map(sid -> Locator.$(new Call(sid)))
+      .filter(Objects::nonNull)
+      .findFirst()
+      .orElseThrow(IllegalArgumentException::new);
+
+    var segment = new Segment(call, Integer.parseInt(request.getParameter("SequenceNumber")));
+    if (!Locator.isRead(segment)) {
+      segment.setCreated(now());
+      Locator.create("VoiceStatus", segment);
+    }
+    switch (request.getParameter("CallbackSource")) {
+      case "call-progress-events" -> update(call, "VoiceStatus", callCopy -> {
+        switch (request.getParameter("CallStatus")) {
+          case "completed" -> {
+            update(segment, "VoiceStatus", segmentCopy -> {
+              segmentCopy.setEnded(now());
+              if (segment.isAnswered()) {
+                callCopy.setTalkTime(Funky.of(call.getTalkTime()).orElse(0L) + segment.getTalkTime());
               }
-              case "answered" -> {
-                update(segment, "VoiceStatus", segmentCopy -> {
-                  segmentCopy.setAnswered(now());
-                });
-              }
-              default -> {
-                callCopy.setResolution(Resolution.DROPPED);
-              }
+            });
+            if (call.getResolution() == null) {
+              callCopy.setResolution(Resolution.DROPPED);
             }
+            callCopy.setDuration(ChronoUnit.SECONDS.between(call.getCreated(), segment.getEnded()));
+          }
+          case "answered" -> update(segment, "VoiceStatus", segmentCopy -> {
+            segmentCopy.setAnswered(now());
           });
+          default -> callCopy.setResolution(Resolution.DROPPED);
         }
-      }
+      });
     }
   }
 

eeeb2452161098fcf22764f27becf0e85965dfcf
Michael Ventura <michael.ventura@gmail.com>
2022-06-11 22:02:08 -0400

    working lead creation from contact

---
+++ src/main/java/com/ameriglide/phenix/api/BusinessModel.java
@@ -0,0 +1,20 @@
+package com.ameriglide.phenix.api;
+
+import com.ameriglide.phenix.common.Business;
+import com.ameriglide.phenix.model.ListableModel;
+import jakarta.servlet.annotation.WebServlet;
+import net.inetalliance.potion.query.Autocomplete;
+import net.inetalliance.potion.query.Query;
+
+@WebServlet("/api/business")
+public class BusinessModel extends ListableModel.Named<Business>{
+  public BusinessModel() {
+    super(Business.class);
+  }
+
+  @Override
+  public Query<Business> search(String query) {
+    return new Autocomplete<>(Business.class,query.split(" "));
+  }
+
+}

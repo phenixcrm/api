0ee89a8e602ef361d6ac40883ace29a4c96dd0a4
Michael Ventura <michael.ventura@gmail.com>
2022-06-21 21:39:13 -0400

    treat VoiceDial's split-personality disorder

--- src/main/java/com/ameriglide/phenix/api/VoiceDial.java
+++ src/main/java/com/ameriglide/phenix/api/VoiceDial.java
@@ -4,35 +4,31 @@ import com.ameriglide.phenix.Auth;
 import com.ameriglide.phenix.Startup;
 import com.ameriglide.phenix.common.Agent;
 import com.ameriglide.phenix.common.Call;
+import com.ameriglide.phenix.servlet.PhenixServlet;
 import com.ameriglide.phenix.servlet.TwiMLServlet;
-import com.ameriglide.phenix.servlet.exception.NotFoundException;
 import com.ameriglide.phenix.types.CallDirection;
 import com.ameriglide.phenix.types.Resolution;
-import com.twilio.http.HttpMethod;
-import com.twilio.twiml.TwiML;
-import com.twilio.twiml.VoiceResponse;
-import com.twilio.twiml.voice.Dial;
-import com.twilio.type.Endpoint;
 import com.twilio.type.PhoneNumber;
 import com.twilio.type.Sip;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import net.inetalliance.log.Log;
 import net.inetalliance.potion.Locator;
 
+import static com.ameriglide.phenix.servlet.TwiMLServlet.asParty;
 import static java.time.LocalDateTime.now;
 import static net.inetalliance.funky.StringFun.isEmpty;
 
-@WebServlet({"/api/twilio/voice/dial", "/twilio/voice/dial"})
-public class VoiceDial extends TwiMLServlet {
+@WebServlet("/api/twilio/voice/dial")
+public class VoiceDial extends PhenixServlet {
 
   @Override
-  protected TwiML getResponse(HttpServletRequest request, HttpServletResponse response) throws Exception {
+  protected void get(HttpServletRequest request, HttpServletResponse response) throws Exception {
     var agent = request.getParameter("agent");
     var number = request.getParameter("number");
     var caller = asParty(Auth.getAgent(request));
-    Party called;
-    Endpoint to;
+    TwiMLServlet.Party called;
     if (isEmpty(agent)) {
       if (isEmpty(number)) {
         throw new IllegalArgumentException();
@@ -41,11 +37,8 @@ public class VoiceDial extends TwiMLServlet {
     } else {
       called = asParty(Locator.$(new Agent(Integer.parseInt(agent))));
     }
-    if (request.getRequestURI().startsWith("/api")) {
       // create new Call with twilio
-
       var from = new Sip(asParty(Auth.getAgent(request)).sip());
-
       var call =
         new Call(Startup.router.call(from, "/twilio/voice/dial", request.getQueryString()).getSid());
       call.setAgent(caller.agent());
@@ -54,40 +47,9 @@ public class VoiceDial extends TwiMLServlet {
       call.setCreated(now());
       call.setResolution(Resolution.ACTIVE);
       Locator.create("VoiceDial", call);
-      info("New API dial %s %s -> %s", call.sid, caller.endpoint(), called.endpoint());
-      return null;
-    } else {
-      // call the caller and then connect to the called party
-      var call = Locator.$(new Call(request.getParameter("CallSid")));
-      if (call == null) {
-        throw new NotFoundException();
-      }
-      if (called.isAgent()) {
-        // internal call
-        return new VoiceResponse.Builder()
-          .say(speak("Connecting you to " + called.agent().getFullName()))
-          .dial(new Dial.Builder()
-            .action("/twilio/voice/postDial")
-            .method(HttpMethod.GET)
-            .answerOnBridge(true)
-            .timeout(15)
-            .sip(buildSip(called))
-            .build())
-          .build();
-      } else {
-        // outbound call
-        return new VoiceResponse.Builder()
-          .say(speak("Dialing the number you requested"))
-          .dial(new Dial.Builder()
-            .action("/twilio/voice/postDial")
-            .method(HttpMethod.GET)
-            .answerOnBridge(true)
-            .timeout(15)
-            .number(buildNumber(asParty(new PhoneNumber(called.endpoint()))))
-            .build())
-          .build();
-      }
-    }
+      log.info("New API dial %s %s -> %s", call.sid, caller.endpoint(), called.endpoint());
+      response.sendError(HttpServletResponse.SC_NO_CONTENT);
 
   }
+  private static final Log log = Log.getInstance(VoiceDial.class);
 }

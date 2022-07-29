56672aeaa50af25b294556c783fb77fe5bced467
Michael Ventura <michael.ventura@gmail.com>
2022-07-28 22:49:55 -0400

    sane credential sync

--- src/main/java/com/ameriglide/phenix/Startup.java
+++ src/main/java/com/ameriglide/phenix/Startup.java
@@ -9,7 +9,7 @@ import jakarta.servlet.annotation.WebListener;
 @WebListener
 public class Startup extends com.ameriglide.phenix.servlet.Startup {
   public Startup() {
-    super(false);
+
   }
 
   @Override

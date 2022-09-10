e675583f18063e8bfacf6662fa954a63868f7003
Michael Ventura <michael.ventura@gmail.com>
2022-09-10 05:41:38 -0400

    use Escaper for TypeModel

--- src/main/java/com/ameriglide/phenix/model/TypeModel.java
+++ src/main/java/com/ameriglide/phenix/model/TypeModel.java
@@ -1,10 +1,9 @@
 package com.ameriglide.phenix.model;
 
 
+import com.ameriglide.phenix.core.Escaper;
 import com.ameriglide.phenix.core.Strings;
 
-import java.net.URLDecoder;
-import java.nio.charset.StandardCharsets;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -20,6 +19,6 @@ public class TypeModel<T> extends Model<T> {
   @Override
   protected Key<T> getKey(final Matcher m) {
     var group = m.group(1);
-    return Key.$(type, Strings.isEmpty(group) ? "" :URLDecoder.decode((m.group(1)), StandardCharsets.UTF_8));
+    return Key.$(type, Strings.isEmpty(group) ? "" :Escaper.url.escape((m.group(1))));
   }
 }

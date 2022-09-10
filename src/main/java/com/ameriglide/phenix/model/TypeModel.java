9245187c86ba70ddb7f0517d0d2b27ad9df1201e
Michael Ventura <michael.ventura@gmail.com>
2022-09-10 04:51:35 -0400

    don't try to decode empty strings

--- src/main/java/com/ameriglide/phenix/model/TypeModel.java
+++ src/main/java/com/ameriglide/phenix/model/TypeModel.java
@@ -1,6 +1,8 @@
 package com.ameriglide.phenix.model;
 
 
+import com.ameriglide.phenix.core.Strings;
+
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
 import java.util.regex.Matcher;
@@ -17,6 +19,7 @@ public class TypeModel<T> extends Model<T> {
 
   @Override
   protected Key<T> getKey(final Matcher m) {
-    return Key.$(type, URLDecoder.decode((m.group(1)), StandardCharsets.UTF_8));
+    var group = m.group(1);
+    return Key.$(type, Strings.isEmpty(group) ? "" :URLDecoder.decode((m.group(1)), StandardCharsets.UTF_8));
   }
 }

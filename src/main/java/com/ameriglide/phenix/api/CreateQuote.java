2335980f795bc58d9d33a2efd121e167d765def0
Michael Ventura <michael.ventura@gmail.com>
2024-02-02 17:14:36 -0500

    add endpoint for attaching beejax quotes to phenix leads

---
+++ src/main/java/com/ameriglide/phenix/api/CreateQuote.java
@@ -0,0 +1,42 @@
+package com.ameriglide.phenix.api;
+
+import com.ameriglide.phenix.common.Opportunity;
+import com.ameriglide.phenix.core.Log;
+import com.ameriglide.phenix.core.Strings;
+import com.ameriglide.phenix.servlet.PhenixServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import net.inetalliance.potion.Locator;
+
+public class CreateQuote extends PhenixServlet {
+  @Override
+  protected void get(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
+    var leadId = request.getParameter("leadId");
+    var code = request.getParameter("code");
+
+    if(Strings.isEmpty("leadId")) {
+      response.sendError(400,"leadId is required");
+      return;
+    }
+    if(Strings.isEmpty("code")) {
+      response.sendError(400, "code is required");
+      return;
+    }
+    try {
+      var id = Integer.parseInt(leadId);
+      var lead = Locator.$(new Opportunity(id));
+      if(lead == null) {
+        response.sendError(404, "could not find lead with id %d".formatted(id));
+      } else {
+        Locator.update(lead,"CreateQuote",copy-> {
+          copy.setQuote(code);
+        });
+      }
+    } catch(NumberFormatException e)  {
+      response.sendError(400, "leadId must be a positive integer");
+      log.error(e);
+    }
+
+  }
+  private static final Log log = new Log();
+}

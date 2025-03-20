9a1f72d2ae132fa7fda92898482764321361d52b
Michael Ventura <michael.ventura@gmail.com>
2025-03-20 11:51:10 -0400

    use the new channelId parameter to reassign lead channels when quotes are attached

--- src/main/java/com/ameriglide/phenix/api/CreateQuote.java
+++ src/main/java/com/ameriglide/phenix/api/CreateQuote.java
@@ -1,7 +1,9 @@
 package com.ameriglide.phenix.api;
 
+import com.ameriglide.phenix.common.Channel;
 import com.ameriglide.phenix.common.Lead;
 import com.ameriglide.phenix.core.Log;
+import com.ameriglide.phenix.core.Optionals;
 import com.ameriglide.phenix.core.Strings;
 import com.ameriglide.phenix.servlet.PhenixServlet;
 import com.ameriglide.phenix.util.Publishing;
@@ -16,6 +18,7 @@ public class CreateQuote extends PhenixServlet {
   protected void get(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
     var leadId = request.getParameter("leadId");
     var code = request.getParameter("code");
+    var channelId = Optionals.of(request.getParameter("channelId")).map(Integer::parseInt).orElse(null);
     if(Strings.isEmpty("leadId")) {
       log.warn(()->"received bad request to attach quote %s to lead %s".formatted(code,leadId));
       response.sendError(400,"leadId is required");
@@ -33,9 +36,20 @@ public class CreateQuote extends PhenixServlet {
         log.warn(()->"received request to attach quote %s to unknown lead %s".formatted(code,leadId));
         response.sendError(404, "could not find lead with id %d".formatted(id));
       } else {
-        log.warn(()->"received request to attach quote %s to lead %s".formatted(code,leadId));
+        var leadChannel = lead.getChannel();
+        var reassignChannel = channelId != null && !channelId.equals(lead.getChannel().id);
+        var newChannel = reassignChannel ? Locator.$(new Channel(channelId)) : leadChannel;
+
+        log.info(()->"attaching quote %s to lead %s (%s)".formatted(code,leadId,lead.getQuoteUrl()));
+        if(reassignChannel) {
+          log.warn(()->"updating channel for lead %s %s->%s (new url: %s)".formatted(leadId,
+            leadChannel.getName(),newChannel.getName(),lead.getQuoteUrl()));
+        }
         Locator.update(lead,"CreateQuote",copy-> {
           copy.setQuote(code);
+          if(reassignChannel) {
+            copy.setChannel(newChannel);
+          }
         });
         Publishing.quoteCreated(lead,log::error);
         response.sendError(200, "OK");

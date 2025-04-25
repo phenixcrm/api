8e961f4a14b4fbd851233e9ce0813ad7eea9f85b
Michael Ventura <michael.ventura@gmail.com>
2025-04-25 14:16:39 -0400

    switch everything to kotlin and used libs.toml

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+  id("com.github.johnrengelman.shadow") version "8.1.1"
+  war
+}
+description = "Phénix objects shared between modules"
+
+dependencies {
+  implementation(project(":common"))
+  implementation(project(":core"))
+  implementation(project(":potion"))
+  implementation(project(":validation"))
+  implementation(project(":types"))
+  implementation(project(":util"))
+  implementation(project(":cli"))
+  implementation(project(":sql"))
+  implementation(libs.csv)
+  implementation(libs.postgresql)
+  implementation(libs.servlet)
+  implementation(libs.websocket)
+  implementation(libs.sendgrid)
+  implementation(libs.twilio)
+  implementation(libs.redis)
+  implementation(libs.dotenv)
+  implementation(libs.bundles.mail)
+  implementation("com.tupilabs:human-name-parser:0.2")
+}
